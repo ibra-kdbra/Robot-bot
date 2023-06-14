834d3970da19fd97ef402e064210291e6542d861
ibra-kdbra <git-repo-develop@outlook.com>
2023-06-14 18:38:08 +0300

    added the user-configuration

---
+++ user-config.py
@@ -0,0 +1,12 @@
+from pywikibot.config import usernames
+
+family = 'wikipedia'
+mylang = 'en'
+
+# usernames['wikipedia']['af'] = 'Rooiratel'
+usernames['wikipedia']['en'] = 'Rooiratel'
+usernames['wikidata']['wikidata'] = 'Rooiratel'
+
+
+
+
